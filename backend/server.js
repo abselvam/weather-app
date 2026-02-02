3f00a72dbc8332cbbd086c316e80dcb018dd69dd
Abhijith Selvam <abselvam44@gmail.com>
2026-02-03 00:35:47 +0530

    initial fetching of weather data implemented

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,52 @@ app.get("/api/weather", async (req, res) => {
   }
 });
 
+const DEFAULT_CITIES = [
+  "London",
+  "New York",
+  "Tokyo",
+  "Paris",
+  "Sydney",
+  "Dubai",
+];
+app.get("/api/weather/multiple", async (req, res) => {
+  const { cities } = req.query;
+  const API_KEY = process.env.API_KEY;
+  const BASE_URL = process.env.BASE_URL;
+
+  // Use provided cities or default ones
+  const cityList = cities ? cities.split(",") : DEFAULT_CITIES;
+
+  try {
+    const promises = cityList.map(async (city) => {
+      try {
+        const response = await fetch(
+          `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city.trim()}&days=1`,
+        );
+
+        if (!response.ok) {
+          console.error(`Failed to fetch weather for ${city}`);
+          return null;
+        }
+
+        const data = await response.json();
+        res.json(data);
+      } catch (error) {
+        console.error(`Error fetching ${city}:`, error);
+        return null;
+      }
+    });
+
+    const results = await Promise.all(promises);
+    const validResults = results.filter((city) => city !== null);
+
+    res.json(validResults);
+  } catch (error) {
+    console.error("Weather api error:", error);
+    res.status(500).json({ error: "error fetching weather data." });
+  }
+});
+
 app.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
 });
---
+++ frontend/src/pages/CityPage.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getWeatherData } from "../api.js";
+import CurrentWeather from "../components/CityPage/CurrentWeather.jsx";
+import HourlyForecast from "../components/CityPage/HourlyForecast.jsx";
+import WeeklyForecast from "../components/CityPage/WeeklyForecast.jsx";
+
+function CityPage() {
+  // const [city, setCity] = useState(initialCity);
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const { city } = useParams();
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      setLoading(true);
+      setError("");
+      try {
+        const data = await getWeatherData(city);
+        const { mintemp_c, maxtemp_c } = data.forecast.forecastday[0].day;
+
+        setWeatherData({
+          current: { ...data.current, mintemp_c, maxtemp_c },
+          hourly: data.forecast.forecastday[0].hour,
+          weekly: data.forecast.forecastday.slice(1),
+          location: data.location,
+        });
+      } catch (error) {
+        console.error("Error fetching weather:", error);
+        setError("Error fetching data: " + error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWeather();
+  }, [city]);
+
+  console.log(weatherData);
+  return (
+    <div className="min-h-screen bg-amber-200 flex items-center justify-center px-10 py-8">
+      <h1>CityPage</h1>
+      <div>
+        {loading && <h1>Loading...</h1>}
+        {error && <h1>{error}</h1>}
+        {weatherData && (
+          <>
+            <CurrentWeather
+              data={weatherData.current}
+              location={weatherData.location}
+            />
+            <HourlyForecast data={weatherData.hourly} />
+            <WeeklyForecast data={weatherData.weekly} />
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default CityPage;
