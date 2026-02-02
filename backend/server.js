0df4abadb56aee83f422cdeefb5dfc19b3f7af85
Abhijith Selvam <abselvam44@gmail.com>
2026-02-02 19:47:36 +0530

    initial commit - project started

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/weather", async (req, res) => {
+  const { city } = req.query;
+  const API_KEY = process.env.API_KEY;
+  const BASE_URL = process.env.BASE_URL;
+  try {
+    const response = await fetch(
+      `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=7`,
+    );
+    if (!response.ok) {
+      console.error("Error in fetching weather api");
+      return res.status(response.status).json({ error: "failed to fetch" });
+    }
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Weateher api error:", error);
+    res.status(500).json({ error: "error fetching weather data." });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+});
